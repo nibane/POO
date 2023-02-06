6812f18c8b8685937e75db8ff898ec81e413a7e8
nibane <azari46165@gmail.com>
2023-02-06 13:36:35 +0100

    changement de nom de dossier plus corection énoncé

--- Action.php
+++ Action.php
@@ -1,4 +1,4 @@
-<h1>Exercice 6<h/1>
+<h1>Exercice 1<h/1>
 
 <p>I.BanqueVous êtes chargé(e) de créer un projet orienté objet permettant de gérer des titulaires et leurs comptes bancaires respectifs.Un compte bancaire est composé des éléments suivants :Un libellé (compte courant, livret A, ...)Un solde initialUne devise monétaireUn titulaire uniqueUn titulaire comporte :UnnomUn prénomUne date de naissanceUne villeL'ensemble de ses comptes bancaires.Sur un compte bancaire, on doit pouvoir :Créditer le compte de X eurosDébiter le compte de X eurosEffectuer un virement d'un compte à l'autre.On doit pouvoir :Affichertoutes  les  informations  d'un(e)  titulaire  (dont  l'âge)  et  l'ensemble  des  comptes appartenant à celui(celle)-ci.Afficher  toutes  les  informations  d'un  compte  bancaire,  notamment  le  nom  /  prénom  du titulaire du compte.</p>
 
@@ -7,6 +7,8 @@
 
 <?php
 
-
+class action{
+    
+}
 
 ?>
