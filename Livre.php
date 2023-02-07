24e51bdf9f54dd765baa5d4d4d6477f4617cb572
nibane <azari46165@gmail.com>
2023-02-07 15:00:13 +0100

    atribut ajouter

---
+++ Livre.php
@@ -0,0 +1,21 @@
+<h1>Exercice 2<h/1>
+
+<p>Les livres sont caractérisés par un titre, un nombre de pages, une année de parution, un prix et un auteur. Un auteur comporte un nom et un prénom.Une méthode toString() sera appréciée dans chacune de vos classes.Vous implémenterez une méthode afficherBibliographie() qui permettra d'afficher la bibliographie complète d'un auteur :</p>
+
+<h2>Résultat</h2>
+
+
+<?php
+
+
+class Livre {
+    private string $titre;
+    private int $nbPage;
+    private DateTime $parution;
+    private int $prix;
+    private string $auteur;
+
+}
+
+
+?>
