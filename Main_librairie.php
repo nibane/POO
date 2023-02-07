9f6924d9e22874ee9067e21246e45665697de11f
nibane <azari46165@gmail.com>
2023-02-07 16:17:34 +0100

    main_auteur changer en main_librairie

---
+++ Main_librairie.php
@@ -0,0 +1,12 @@
+<h1>Exercice 2<h/1>
+
+<p>Vous êtes chargé(e) de créer un projet orienté objet permettant de gérer des livres et leurs auteurs respectifs.Les livres sont caractérisés par un titre, un nombre de pages, une année de parution, un prix et un auteur. Un auteur comporte un nom et un prénom.Une méthode toString() sera appréciée dans chacune de vos classes.Vous implémenterez une méthode afficherBibliographie() qui permettra d'afficher la bibliographie complète d'un auteur :</p>
+
+<h2>Résultat</h2>
+
+
+<?php
+
+
+
+?>
