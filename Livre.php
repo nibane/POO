d2b89d08dcce7698d3ed09f646b5ec7b0030187a
nibane <azari46165@gmail.com>
2023-02-08 09:08:38 +0100

    contenue balise <p> retirer

--- Livre.php
+++ Livre.php
@@ -1,6 +1,6 @@
 <h1>Exercice 2<h/1>
 
-<p>Les livres sont caractérisés par un titre, un nombre de pages, une année de parution, un prix et un auteur. Un auteur comporte un nom et un prénom.Une méthode toString() sera appréciée dans chacune de vos classes.Vous implémenterez une méthode afficherBibliographie() qui permettra d'afficher la bibliographie complète d'un auteur :</p>
+<p></p>
 
 <h2>Résultat</h2>
 
