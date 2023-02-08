087f3a810b224b82a287e37c2912aaf8921da1d6
nibane <azari46165@gmail.com>
2023-02-08 13:52:37 +0100

    ajout atribut function __construct

--- Réalisateur.php
+++ Réalisateur.php
@@ -1,7 +1,19 @@
 <?php
 
 class Real{
+    private string $nom;
+    private string $prenom;
+    private string $sexe;
+    private DateTime $naissance;
+    private array $allFilm;
 
+    function __construct(string $nom, string $prenom, string $sexe, string $naissance,){
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->sexe = $sexe;
+        $this->naissance = new DateTime($naissance);
+        $this->allFilm = [];
+    }
 
 
 
