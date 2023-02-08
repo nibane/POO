7df8ff1bb77793ac0c881ce01e29e8adf0771c84
nibane <azari46165@gmail.com>
2023-02-08 10:16:46 +0100

    ajout de commande permetant d'afficher les element demander

--- Main_librairie.php
+++ Main_librairie.php
@@ -14,10 +14,17 @@ spl_autoload_register(function ($class_name) {
 
 $A1 = new Auteur("Stephen", "King", "https://www.senscritique.com/liste/stephen_king_e_bibliographie_complete/48872");
 
-echo $A1;
+echo $A1." ".$A1->getBiblio()."<br>";
 
 $L1 = new Livre("Ca", 1138, "1986", 20, $A1);
+echo $L1."<br><br>";
 
-echo $L1;
+$L2 = new Livre("Simetierre", 374, "1983", 15 , $A1);
+$L3 = new Livre("Le Fléau", 823, "1978", 14, $A1);
+$L4 = new Livre("Shining", 447, "1977", 16, $A1);
+
+echo "liste des livre<br>";
+
+$A1->tlivre();
 
 ?>
