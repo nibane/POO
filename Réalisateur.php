a94b9f8c209ac376d3c53b4c2fc8202d937ef8a0
nibane <azari46165@gmail.com>
2023-02-08 14:16:11 +0100

    ajout getter setter , corection function ajouterFilm

--- Réalisateur.php
+++ Réalisateur.php
@@ -5,16 +5,50 @@ class Real{
     private string $prenom;
     private string $sexe;
     private DateTime $naissance;
-    private array $allFilm;
+    private array $allFilms;
 
     function __construct(string $nom, string $prenom, string $sexe, string $naissance,){
         $this->nom = $nom;
         $this->prenom = $prenom;
         $this->sexe = $sexe;
         $this->naissance = new DateTime($naissance);
-        $this->allFilm = [];
+        $this->allFilms = [];
+    }
+    function set_nom($nom){
+        $this->nom = $nom;
     }
 
+    function get_nom(){
+        return $this->nom;
+    }
+    function set_prenom($prenom){
+        $this->prenom = $prenom;
+    }
+
+    function get_prenom(){
+        return $this->prenom;
+    }
+    function set_sexe($sexe){
+        $this->sexe = $sexe;
+    }
+
+    function get_sexe(){
+        return $this->sexe;
+    }
+    function set_naissance($naissance){
+        $this->naissance = $naissance;
+    }
+
+    function get_naissance(){
+        return $this->naissance;
+    }
+    function set_allFilms($allFilms){
+        $this->allFilms = $allFilms;
+    }
+
+    function get_allFilms(){
+        return $this->allFilms;
+    }
 
 
 
@@ -27,8 +61,8 @@ class Real{
 
 
 
-    public function ajouterFilm(Film $film){
-        $this->film[] = $film;
+    public function ajouterFilm(Film $allFilms){
+        $this->allFilms[] = $allFilms;
     }
 
 }
