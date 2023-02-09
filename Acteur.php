6cedcf858ed791a633656c722cdabc9dc77138cb
nibane <azari46165@gmail.com>
2023-02-09 15:36:50 +0100

    getter est setter ajouter

--- Acteur.php
+++ Acteur.php
@@ -15,6 +15,28 @@ class Acteur{
         
     }
 
+    function set_nom($nom){
+        $this->nom = $nom;
+    }
+    
+    function get_nom(){
+        return $this->nom;
+    }
+    function set_prenom($prenom){
+        $this->prenom = $prenom;
+    }
+    
+    function get_prenom(){
+        return $this->prenom;
+    }
+    function set_castings($castings){
+        $this->castings = $castings;
+    }
+    
+    function get_castings(){
+        return $this->castings;
+    }
+
 
     function addCasting(Casting $casting) {
         $this->castings[] = $casting;
