5298f9bfe85c3dec2b1b71d2331623b46ad6251b
nibane <azari46165@gmail.com>
2023-02-09 15:36:35 +0100

    getter est setter ajouter au atribut

--- Role.php
+++ Role.php
@@ -12,6 +12,21 @@ class Role {
     $this->castings = [];
     }
 
+    function set_nomRole($nomRole){
+        $this->nomRole = $nomRole;
+    }
+    
+    function get_nomRole(){
+        return $this->nomRole;
+    }
+    function set_castings($castings){
+        $this->castings = $castings;
+    }
+    
+    function get_castings(){
+        return $this->castings;
+    }
+
     function addCasting(Casting $casting) {
         $this->castings[] = $casting;
     }
