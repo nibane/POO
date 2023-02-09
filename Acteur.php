22e5f22fe7dae05cad60fcfaa8c9560d9caee749
nibane <azari46165@gmail.com>
2023-02-09 14:48:06 +0100

    ajout class Auteur ,atribut $nom $prenom et $castings qui est un tableau ajouter avec function __construct plus ajout function addCasting permet d'ajouter des element dans le tableau castings

--- Acteur.php
+++ Acteur.php
@@ -1,8 +1,28 @@
+
 <?php
 
-class Acteur{
+class Acteur{  
+    
+    private string $nom;
+    private string $prenom;
+    private array $castings;
 
+    function __construct(string $nom, string $prenom)
+    {
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->castings = [];
+        
+    }
+
+
+    function addCasting(Casting $casting) {
+        $this->castings[] = $casting;
+    }
+    
+
+   
 }
 
-
+//casting
 ?>
