0444bacec1e42a96fe23c9c7901aa7de2e050ad0
nibane <azari46165@gmail.com>
2023-02-09 14:41:45 +0100

    ajout class role avec 2 atribut dont un tableau , function construct , function addCasting() qui rajoute des objet venant de la classe casting()

--- Role.php
+++ Role.php
@@ -1,5 +1,26 @@
+
 <?php
 
+class Role {
+    
+    private string $nomRole;
+    private array $castings;
+    
+    function __construct(string $nomRole )
+    {
+    $this->nomRole = $nomRole;
+    $this->castings = [];
+    }
 
+    function addCasting(Casting $casting) {
+        $this->castings[] = $casting;
+    }
+    
+
+
+}
+
+
+//casting
 
 ?>
